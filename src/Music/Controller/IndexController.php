17544efd83403a4e91d8cb9facd9c6d29637556f
Einar Valur Aðalsteinsson <einar@einars-workstation.lan>
2013-03-24 12:48:37 +0000

    added JSON support

--- src/Music/Controller/IndexController.php
+++ src/Music/Controller/IndexController.php
@@ -4,10 +4,11 @@ namespace Music\Controller;
 
 use DateTime;
 
-use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Mvc\Controller\AbstractRestfulController;
 use Zend\View\Model\ViewModel;
+use Zend\View\Model\JsonModel;
 
-class IndexController extends AbstractActionController{
+class IndexController extends AbstractRestfulController{
 	
 	public function indexAction(){
         return new ViewModel(array(
@@ -24,25 +25,36 @@ class IndexController extends AbstractActionController{
         );
 	}
     
+    /**
+     * Get a range og artists
+     */
     public function timeRangeAction(){
-        return new ViewModel(array(
-        	'range' => (object)array(
-        		'previous' => ((int)$this->params()->fromRoute('from',2011))-1,
-        		'from' => ((int)$this->params()->fromRoute('from',2011)),
-        		'to' => ((int)$this->params()->fromRoute('to',null)),
-        		'next' => ((int)$this->params()->fromRoute('from',2011))+1,
-        	),
+        $data = array(
+            'range' => (object)array(
+                'previous' => ((int)$this->params()->fromRoute('from',2011))-1,
+                'from' => ((int)$this->params()->fromRoute('from',2011)),
+                'to' => ((int)$this->params()->fromRoute('to',null)),
+                'next' => ((int)$this->params()->fromRoute('from',2011))+1,
+            ),
             'collection' => $this->getServiceLocator()
                 ->get('Music\Model\Music')->fetchRange(
-                	$this->params()->fromRoute('from', null),
-                	$this->params()->fromRoute('to', null)
+                    $this->params()->fromRoute('from', null),
+                    $this->params()->fromRoute('to', null)
                 )
-        	)
-        );
+            );
+
+        if(preg_match("/application\/json/", $this->getRequest()->getHeader("Accept")->getFieldValue())){
+            return new JsonModel($data);
+        }else{
+            return new ViewModel($data);
+        }
     }
 
+    /**
+     * Get profile of one artist
+     */
     public function profileAction(){
-    	return new ViewModel(array(
+    	$data = array(
     		'value' => (object)array(
     			'raw' 		=> $this->params()->fromRoute('name', null),
     			'decoded' 	=> urldecode($this->params()->fromRoute('name', null)),
@@ -52,7 +64,19 @@ class IndexController extends AbstractActionController{
                     urldecode($this->params()->fromRoute('name', null)),
                 	$this->params()->fromRoute('type', null)
                 )
-        	)
-    	);
+        	);
+
+
+        if(preg_match("/application\/json/", $this->getRequest()->getHeader("Accept")->getFieldValue())){
+            return new JsonModel($data);
+        }else{
+            return new ViewModel($data);
+        }
     }
+
+    public function get($id){}
+    public function getList(){}
+    public function update($id, $data){}
+    public function create($date){}
+    public function delete($id){}
 }
